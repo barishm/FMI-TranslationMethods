35de45ec565e39a43be1579835d7510150c3314a
barishm <barishm@outlook.com>
2024-01-28 17:15:34 +0200

    text editor implemented

---
+++ src/TextEditor.java
@@ -0,0 +1,132 @@
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class TextEditor extends JFrame implements ActionListener {
+    JTextArea textArea;
+    JScrollPane scrollPane;
+
+    JMenuBar menuBar;
+    JMenu fileMenu;
+    JMenu runMenu;
+    JMenuItem openItem;
+    JMenuItem saveItem;
+    JMenuItem exitItem;
+    JMenuItem runItem;
+    public TextEditor() throws HeadlessException {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("Translator");
+        this.setSize(600,480);
+        this.setLayout(new FlowLayout());
+        this.setLocationRelativeTo(null);
+        textArea = new JTextArea();
+        textArea.setLineWrap(true);
+        textArea.setWrapStyleWord(true);
+        textArea.setFont(new Font("Arial",Font.PLAIN,16));
+        scrollPane = new JScrollPane(textArea);
+        scrollPane.setPreferredSize(new Dimension(575,410));
+        scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+
+        // ----Menu Bar----
+        menuBar = new JMenuBar();
+        fileMenu = new JMenu("File");
+        runMenu = new JMenu("Run");
+        openItem = new JMenuItem("Open");
+        saveItem = new JMenuItem("Save");
+        exitItem = new JMenuItem("Exit");
+        runItem = new JMenuItem("Run...");
+
+        openItem.addActionListener(this);
+        saveItem.addActionListener(this);
+        exitItem.addActionListener(this);
+        runItem.addActionListener(this);
+
+        fileMenu.add(openItem);
+        fileMenu.add(saveItem);
+        fileMenu.add(exitItem);
+        runMenu.add(runItem);
+        menuBar.add(fileMenu);
+        menuBar.add(runMenu);
+
+        // ----/Menu Bar----
+
+        this.setJMenuBar(menuBar);
+        this.add(scrollPane);
+        this.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource()==openItem) {
+            JFileChooser fileChooser = new JFileChooser();
+            fileChooser.setCurrentDirectory(new File("."));
+            FileNameExtensionFilter filter = new FileNameExtensionFilter("Text files", "txt");
+            fileChooser.setFileFilter(filter);
+
+            int response = fileChooser.showOpenDialog(null);
+
+            if(response == JFileChooser.APPROVE_OPTION) {
+                File file = new File(fileChooser.getSelectedFile().getAbsolutePath());
+                Scanner fileIn = null;
+
+                try {
+                    fileIn = new Scanner(file);
+                    if(file.isFile()) {
+                        textArea.setText("");
+                        while(fileIn.hasNextLine()) {
+                            String line = fileIn.nextLine()+"\n";
+                            textArea.append(line);
+                        }
+                    }
+                } catch (FileNotFoundException e1) {
+                    e1.printStackTrace();
+                }
+                finally {
+                    fileIn.close();
+                }
+            }
+        }
+        if (e.getSource() == saveItem) {
+            JFileChooser fileChooser = new JFileChooser();
+            fileChooser.setCurrentDirectory(new File("."));
+
+            FileNameExtensionFilter filter = new FileNameExtensionFilter("Text files (*.txt)", "txt");
+            fileChooser.setFileFilter(filter);
+
+            int response = fileChooser.showSaveDialog(null);
+
+            if (response == JFileChooser.APPROVE_OPTION) {
+                File file;
+                PrintWriter fileOut = null;
+
+                file = fileChooser.getSelectedFile();
+
+                // Append .txt extension if not already present
+                if (!file.getName().toLowerCase().endsWith(".txt")) {
+                    file = new File(file.getAbsolutePath() + ".txt");
+                }
+
+                try {
+                    fileOut = new PrintWriter(file);
+                    fileOut.println(textArea.getText());
+                } catch (FileNotFoundException e1) {
+                    e1.printStackTrace();
+                } finally {
+                    if (fileOut != null) {
+                        fileOut.close();
+                    }
+                }
+            }
+        }
+        if(e.getSource()==exitItem) {
+            System.exit(0);
+        }
+
+    }
+}
